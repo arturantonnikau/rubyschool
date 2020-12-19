1f29e7a2e88178dda9046230726be04e1bb03bc1
arturantonnikau <arturantonnikau@gmail.com>
2020-12-19 23:14:47 +0300

    add admin

--- app1.rb
+++ app1.rb
@@ -4,6 +4,25 @@ get "/" do
   erb :index
 end
 
+get "/admin" do
+  erb :admin
+end
+
+post "/admin" do
+  @user = params[:user].strip
+  @pass = params[:pass].strip
+
+  @file = File.open("users.txt", "r")
+
+  if @user == "archi" && @pass == "301191"  
+    erb :file
+  else
+    @file.close
+    @fail_access = "Sorry, this is invalid user or password!"
+    erb :file
+  end
+end
+
 post "/" do
   @user_name = params[:user_name]
   @phone = params[:phone]
@@ -12,8 +31,8 @@ post "/" do
   @title = "Thank you!"
   @message = "Dear #{@user_name}, we`ll be waiting for you at #{@date_time} "
 
-  file = File.open("users.txt", "w")
-  file.write("#{@user_name},#{@phone},#{@date_time}")
+  file = File.open("users.txt", "a")
+  file.write("#{@user_name},#{@phone},#{@date_time}\n")
   file.close
 
   erb :message
